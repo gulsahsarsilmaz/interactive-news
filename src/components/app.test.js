d73863c5feb0d42f6dba87866fa33d6fa408d4f1
Gulsah Sarsilmaz <sarsilmazgulsah@gmail.com>
2020-08-21 11:21:57 -0400

    fixing typo

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './app';\
+import App from './app';
 
 import '@testing-library/jest-dom/extend-expect';
 
